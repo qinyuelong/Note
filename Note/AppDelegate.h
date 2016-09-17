6acea0deab68dadcd228b6259aa02aea922180f5
qinge <yuelong.qin@monstar-lab.com>
2016-09-17 14:23:13 +0800

    init project and add MMDrawerController

---
+++ Note/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Note
+//
+//  Created by qinge on 16/9/17.
+//  Copyright © 2016年 qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Note/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Note
+//
+//  Created by qinge on 16/9/17.
+//  Copyright © 2016年 qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
